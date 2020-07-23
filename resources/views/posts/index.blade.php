ba10187bc68f5800ade82deb6393f65cd0be7fcf
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-23 18:00:22 +0500

    Showing data from dB into view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,23 +3,26 @@
 @section('content')
     <h1>Post</h1>
     <?php
-        if(count($posts)){
-            foreach ($posts as $post) {
+        // if(count($posts)){
+        //     foreach ($posts as $post) {
                 ?>
-            <div class="jumbotron">
+            {{-- <div class="jumbotron">
                 <p>wow</p>
-            </div>
+            </div> --}}
                  <?php
-            }
-        }
-        else{
-            echo "<h2>No Post Found</h2>";
-        }
+        //     }
+        // }
+        // else{
+        //     echo "<h2>No Post Found</h2>";
+        // }
     ?>
     @if (count($posts))
-        <div class="jumbotron">
-
-        </div>
+        @foreach ($posts as $post)
+            <div class="jumbotron">
+                <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
+                <small>written on {{ $post->created_at }}</small>
+            </div>
+        @endforeach
     @else
         <p>No Post Found</p>
     @endif
