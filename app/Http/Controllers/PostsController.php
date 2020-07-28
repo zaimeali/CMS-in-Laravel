0ea6077fc3dde7084621911af9f12bc309ec2923
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 18:17:47 +0500

    Post Deleting Successfully

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -106,6 +106,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Successfully Deleted!');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,16 @@
     <small>Written on {{ $post->created_at }}</small>
     <hr>
     <a href="/posts/{{ $post->id }}/edit" class="btn btn-secondary">Edit</a>
+    {!! Form::open([
+        'action' => ['PostsController@destroy', $post->id], 
+        'method' => 'POST',
+        'class' => 'float-right'
+        ]) 
+    !!}
+
+        {{ Form::hidden('_method', 'DELETE') }}
+
+        {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+
+    {!! Form::close() !!}
 @endsection
