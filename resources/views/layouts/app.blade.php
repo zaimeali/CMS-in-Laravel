1cca99ec02935698d8a10b59ce49e59e2a02bd6f
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 17:49:23 +0500

    CKEditor Cdn Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,9 @@
         </div>
         
 
+        {{-- CKEditor CDN --}}
+        <script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+
         {{-- Bootstrap JS --}}
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
             integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" 
