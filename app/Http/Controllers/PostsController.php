b3bba30ca7d4a27438586a935534dd375e513f2e
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 18:11:26 +0500

    Post Editing Successfully

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -71,7 +71,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -83,7 +84,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+
+        $post->save();
+
+        return redirect('/posts/'.$id)->with('success', 'Post Successfully Edited!');
     }
 
     /**
