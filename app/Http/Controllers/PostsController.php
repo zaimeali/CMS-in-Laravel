93a60168a848b1f7b0f111dc26cd8feb6fe03f3c
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 16:27:46 +0500

    Single Post Showing

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -36,7 +36,7 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -47,7 +47,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->with('post', $post);
     }
 
     /**
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="/post" class="btn btn-primary">Go Back</a>
+    <h1>{{ $post->title }}</h1>
+    <div>
+        {{ $post->body }}
+    </div>
+    <hr>
+    <small>Written on {{ $post->created_at }}</small>
+@endsection
