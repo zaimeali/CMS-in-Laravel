419093686b782fb98e46285b163946fdb4b3d5ce
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-28 17:32:11 +0500

    Form Created

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,4 +4,31 @@
 
     <h1>Create Post</h1>
 
+    {{ Form::open(['action' => 'PostsController@store', 'method' => 'POST']) }}
+
+        <div class="form-group">
+            {{ Form::label('title', 'Title') }}
+            {{ Form::text(
+                'title', 
+                '', 
+                [
+                    'class' => 'form-control', 
+                    'placeholder' => 'Enter Title'
+                ]) 
+            }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('body', 'Body') }}
+            {{ Form::text(
+                'body', 
+                '', 
+                [
+                    'class' => 'form-control', 
+                    'placeholder' => 'Enter Body Text'
+                ]) 
+            }}
+        </div>
+
+        {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+    {{ Form::close() }}
 @endsection
